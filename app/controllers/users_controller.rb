9d66177537ec18547b8b5c888b3212540c7da1cc
sagaatusshi <100151677+sagaatusshi@users.noreply.github.com>
2022-03-13 01:23:12 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).parmit[:name, :email]
+    params.require(:user).permit(:name, :email)
   end
 
 end
